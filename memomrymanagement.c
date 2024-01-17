dfb9c6aab30bb8473f420b8ba7b61a0e54c9cd68
Olamilekan2017 <sodiqolayinka2017@gmail.com>
2024-01-17 13:24:01 -0800

    memory_management

---
+++ memomrymanagement.c
@@ -0,0 +1,18 @@
+/**
+ * free_stack - Frees all nodes in doubly linked list
+ * @head: pointer to the head
+ * Return: it is void
+ */
+
+void free_stack(stack_t *head)
+{
+	stack_t *currenthd;
+
+	currenthd = head;
+	while (head != NULL)
+	{
+		currenthd = head->next;
+		free(head);
+		head = currenthd;
+	}
+}
